b6559cd8b879528d524a2dc4928b7c8c819cb730
arkhannanov <arkhannanov@gmail.com>
2019-04-10 20:37:24 +0300

    Form finished + TOGGLES (HTML+CSS) finished

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './js/button.js'
 import './js/slider1.js'
 import './js/slider2.js'
 import './js/stages.js'
+import './js/form.js'
+//import './js/postcss.config.js'
 
 //import './css/main.css'
 import './scss/main.scss'
