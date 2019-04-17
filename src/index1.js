a47396ec0941a36d6e4bdd76a3731a2bab8895cd
arkhannanov <arkhannanov@gmail.com>
2019-04-17 20:14:19 +0300

    index1.html continue editing

--- src/index1.js
+++ src/index1.js
@@ -3,6 +3,8 @@
 //import './js/postcss.config.js'
 //import './css/main.css'
 import './scss/index1.scss'
+import './js/button.js'
+import './js/calendar.js'
 
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
