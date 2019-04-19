812c387d61c8b975131338e437cf3c918224afce
arkhannanov <arkhannanov@gmail.com>
2019-04-19 17:03:41 +0300

    index2.html finished

--- src/index2.js
+++ src/index2.js
@@ -5,6 +5,9 @@
 import './scss/index2.scss'
 import './js/stages.js'
 import './js/button.js'
+//import './js/tick_boxes.js'
+import './js/toggles.js'
+import './js/form2.js'
 
 
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
