e32fd85d46c98d5ba6b7f5b67bb8abf51189816b
arkhannanov <arkhannanov@gmail.com>
2019-07-10 08:25:20 +0300

    Refactoring #9

---
+++ src/_js/favicons.js
@@ -0,0 +1,7 @@
+const faviconsContext = require.context(
+    '!!file-loader?name=./image/favicons/[name].[ext]!.',
+    true,
+    /\.(svg|png|ico|xml|json)$/
+  );
+  
+  faviconsContext.keys().forEach(faviconsContext);
