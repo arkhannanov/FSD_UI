e396aae80985d686c2c1711eec72cf6dd10a3163
arkhannanov <arkhannanov@gmail.com>
2019-07-18 10:14:39 +0300

    Refactoring #12 ripple - элемент button

--- src/blocks/button/button.js
+++ src/blocks/button/button.js
@@ -15,11 +15,11 @@ class Button {
         circle.style.width = `${diametr}px`;
         circle.style.height = `${diametr}px`;
         circle.style.left = `${elem.clientX - this.offsetLeft + window.pageXOffset - diametr / 2}px`;
-        circle.style.top = `${elem.clientY - this.offsetTop + window.pageYOffset - diametr / 2}px`;
-        circle.classList.add('ripple');
+        circle.style.top = `${elem.clientY - this.offsetTop + window.pageYOffset - 80 - diametr / 2}px`;
+        circle.classList.add('button__ripple');
         setTimeout(() => {
             this.removeChild(circle);
-        }, 2000);
+        }, 10000);
     }
 }
 
