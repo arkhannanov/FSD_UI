697174d55a02007a35f2db23c7689be848a97334
arkhannanov <arkhannanov@gmail.com>
2019-08-05 22:29:11 +0300

    Refactoring #16 Использовал drop-down компонену на rent-auto страницы. Дописал необходимый js код.

---
+++ src/blocks/drop-down/drop-down.js
@@ -0,0 +1,29 @@
+class DropDown {
+  constructor(root) {
+    this.root = root;
+    this.targetString;
+    this.enableOptions();
+  }
+
+  enableOptions() {
+
+    this.targetString = this.root.childNodes[1].children[0];
+
+    const text = this.targetString.innerHTML;
+
+    this.targetString.addEventListener('click', () => {
+      this.targetString.innerHTML = text;
+    });
+    for (let i = 0; i < this.root.childNodes[3].children.length; i += 1) {
+      this.root.childNodes[3].children[i].addEventListener('click', () => {
+        this.targetString.innerHTML = this.root.childNodes[3].children[i].innerHTML;
+      });
+    }
+  }
+}
+
+const root = document.getElementsByClassName('drop-down');
+
+for (let i = 0; i < root.length; i += 1) {
+  new DropDown(root[i]);
+}
