9a455987b5038f578a74fb3a50e4baf2ff5b6099
arkhannanov <arkhannanov@gmail.com>
2019-08-19 11:59:35 +0300

    Refactoring #9 Исправлено location на классы.

--- src/blocks/location/location.js
+++ src/blocks/location/location.js
@@ -1,27 +1,45 @@
 class Location {
-  constructor(myMap) {
-    this.createGeoObjects(myMap);
+  constructor(root) {
+    this.root = root;
+    this.myMap;
+    this.createGeoObjects();
   }
 
-  createGeoObjects(myMap) {
-    const myGeoObjects = new ymaps.Placemark([37.787509, -122.444838], {
-      balloonContentBody: 'Location',
-    }, {
-      iconLayout: 'default#image',
-      iconImageHref: './src/blocks/location/images/location-icon.png',
-      iconImageSize: [41, 56],
-      iconImageOffset: [-35, -35]
+  createMyMap(map) {
+    this.myMap = new map.Map(this.root, {
+      center: [37.787509, -122.444838],
+      zoom: 13,
+      controls: [],
     });
+  }
 
-    const clusterer = new ymaps.Clusterer({
-      clusterDisableClickZoom: false,
-      clusterOpenBalloonOnClick: false,
+  createGeoObjects() {
+    ymaps.load().then((map) => {
+      this.createMyMap(map);
+
+      const myGeoObjects = new map.Placemark([37.787509, -122.444838], {
+        balloonContentBody: 'Location',
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: './src/blocks/location/images/location-icon.png',
+        iconImageSize: [41, 56],
+        iconImageOffset: [-35, -35]
+      });
+
+      const clusterer = new map.Clusterer({
+        clusterDisableClickZoom: false,
+        clusterOpenBalloonOnClick: false,
+      });
+
+      clusterer.add(myGeoObjects);
+      this.myMap.geoObjects.add(clusterer);
+      this.myMap.behaviors.disable('scrollZoom');
     });
-
-    clusterer.add(myGeoObjects);
-    myMap.geoObjects.add(clusterer);
-    myMap.behaviors.disable('scrollZoom');
   }
 }
 
 export default Location;
+
+const root = document.getElementsByClassName('js-location__map');
+
+Array.prototype.forEach.call(root, item => new Location(item));
