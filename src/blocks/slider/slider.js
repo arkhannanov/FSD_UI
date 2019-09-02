bc085506b0115071ee908da19d3adbba22a5c678
arkhannanov <arkhannanov@gmail.com>
2019-09-02 18:51:16 +0300

    Промежуточный результат

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -11,13 +11,15 @@ class Slider {
     const max = this.root.data('max');
     const step = (this.root.data('max') - this.root.data('min')) / 4;
 
+    console.log(this.root.parent().parent()[0].offsetWidth);
+
     this.root.jRange({
       from: this.root.data('min'),
       to: this.root.data('max'),
       step: this.root.data('step'),
       scale: [min, min + step, min + 2 * step, min + 3 * step, max],
       format: '%s',
-      width: 260,
+      width: this.root.parent().parent()[0].offsetWidth,
       showLabels: this.root.data('label'),
       snap: true,
       theme: `theme-${this.root.data('color')}`,
@@ -29,6 +31,7 @@ class Slider {
 }
 
 const $sliders = $('.js-newslider');
+
 
 $sliders.each(function createSlider() {
   new Slider(this);
